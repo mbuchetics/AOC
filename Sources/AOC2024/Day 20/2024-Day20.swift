56e100c0993872cb84662535690dfebcf8c12433
Matthias Buchetics <mbuchetics@gmail.com>
2024-12-01 10:25:07 +0100

    updated for 2024

---
+++ Sources/AOC2024/Day 20/2024-Day20.swift
@@ -0,0 +1,29 @@
+//
+//  Day20.swift
+//  AOC2024
+//
+//  Created by Dave DeLong on 12/1/24.
+//  Copyright © 2024 Dave DeLong. All rights reserved.
+//
+
+struct Day20: Day {
+    typealias Part1 = String
+    typealias Part2 = String
+    
+    static var rawInput: String? { nil }
+
+    func run() async throws -> (Part1, Part2) {
+        let p1 = try await part1()
+        let p2 = try await part2()
+        return (p1, p2)
+    }
+
+    func part1() async throws -> Part1 {
+        return #function
+    }
+
+    func part2() async throws -> Part2 {
+        return #function
+    }
+
+}
